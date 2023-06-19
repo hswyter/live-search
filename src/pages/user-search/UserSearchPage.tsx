460a27cc16f628a818e248ba2c39c933a9825f07
Hauke Swyter <17335964+hswyter@users.noreply.github.com>
2023-06-19 13:31:46 +0200

    Debounce user input

--- src/pages/user-search/UserSearchPage.tsx
+++ src/pages/user-search/UserSearchPage.tsx
@@ -3,18 +3,27 @@ import PageLayout from '../../components/layout/PageLayout';
 import SearchInputField from '../../components/ui/atoms/input/SearchInputField';
 import { UserSearchProvider } from '../../components/context/UserSearchContext';
 import UserSearchResult from './UserSearchResult';
+import { debounce } from 'lodash';
 
 const UserSearchPage: FunctionComponent = () => {
+  const [searchInputValue, setSearchInputValue] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleDebounceFn = (value: string) => {
+    setSearchTerm(value);
+  }
+
+  const debounceFn = useCallback(debounce(handleDebounceFn, 200), []);
+
   const handleSearchInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.currentTarget.value);
+    setSearchInputValue(e.currentTarget.value);
+    debounceFn(e.currentTarget.value);
   }, []);
 
   return (
     <UserSearchProvider searchTerm={searchTerm}>
       <PageLayout>
-        <SearchInputField onChange={handleSearchInputChange} value={searchTerm} />
+        <SearchInputField onChange={handleSearchInputChange} value={searchInputValue} />
         <UserSearchResult />
       </PageLayout>
     </UserSearchProvider>
